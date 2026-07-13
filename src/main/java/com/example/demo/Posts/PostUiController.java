caf3c0b9a233ffd49ffb9485f1c8f5a5db512cce
RJ-885 <rtjohnson@uncg.edu>
2026-07-13 13:04:34 -0400

    Changes to be committed: 	modified:   src/main/java/com/example/demo/Posts/PostUiController.java 	modified:   src/main/resources/templates/about.ftlh 	modified:   src/main/resources/templates/details.ftlh

--- src/main/java/com/example/demo/Posts/PostUiController.java
+++ src/main/java/com/example/demo/Posts/PostUiController.java
@@ -1,15 +1,29 @@
 package com.example.demo.Posts;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 
 @Controller
 public class PostUiController {
-    
+
     private final PostService postService;
 
     public PostUiController(PostService postService) {
         this.postService = postService;
     }
 
-    
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/details/{characterId}")
+    public String getPostById(@PathVariable long characterId, Model model) {
+        Post character = postService.getPostById(characterId);
+        model.addAttribute("character", character);
+
+        return "details";
+    }
 }
